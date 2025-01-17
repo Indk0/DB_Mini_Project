42e570cc342c9e24ec6e3e4e005256fec8730e6e
Inder Kumar <inderik302@gmail.com>
2025-01-17 10:13:28 +0000

    Fix css styling issue

--- collections_app/urls.py
+++
@@ -1,9 +0,0 @@
-# from . import views
-# from collections_app import views
-
-# urlpatterns = [
-#     path('collectornotes/', views.collectors_notes, name='notes'),
-#     path('customertransactions/', views.customer_transaction,
-#          name='transactions'),
-#     path('plans/', views.plans, name='plans'),
-# ]
