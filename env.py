d7da36527adccedf6208b58b2e250bbefdfc7a83
Inder Kumar <inderik302@gmail.com>
2025-01-17 10:56:36 +0000

    Config env.py file

--- env.py
+++ env.py
@@ -3,3 +3,13 @@ import os
 os.environ.setdefault("SECRET_KEY", 'j7lthp&5n7wkwx&%k)+as7e6wacl$j52r#mv118)#du%yir%pn')
 
 os.environ.setdefault("ENGINE", 'django.db.backends.mysql')
+
+os.environ.setdefault('NAME', 'collections')
+
+os.environ.setdefault('USER', 'root')
+
+os.environ.setdefault('PASSWORD', 'gen44')
+
+os.environ.setdefault('HOST', '127.0.0.1')
+
+os.environ.setdefault('PORT', '3306')
